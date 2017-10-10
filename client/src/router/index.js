6b89157182d846b1430bb8bd0962a85e0b88c038
yangxuechuan <745886214@qq.com>
2017-10-10 18:25:57 +0800

    vnshop

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,13 +2,35 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import GoodsList from '@/view/GoodsList'
+// import User from '@/view/user/index'
+import Cart from '@/view/Cart'
+import Address from '@/view/Address'
+import OrderConfirm from '@/view/OrderConfirm'
+import OrderSuccess from '@/view/OrderSuccess'
 
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-        path: '/',
-        name: 'GoodsList',
-        component: GoodsList
-    }]
+            path: '/',
+            name: 'GoodsList',
+            component: GoodsList
+        },
+        {
+            path: '/cart', 
+            component: Cart
+        },
+        {
+            path:'/address',
+            component: Address
+        },
+        {
+            path: '/orderConfirm',
+            component: OrderConfirm
+        },
+        {
+            path: '/orderSuccess',
+            component: OrderSuccess
+        }
+    ]
 })
